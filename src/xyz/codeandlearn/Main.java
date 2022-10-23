469334d3e40e2417c4c44559609ab7838354d900
Daniele Vinci <vinci.dani@gmail.com>
2022-10-22 21:01:08 -0400

    init

---
+++ src/xyz/codeandlearn/Main.java
@@ -0,0 +1,9 @@
+package xyz.codeandlearn;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello JAVA!");
+        System.out.println();
+        System.out.println(10 + 10);
+    }
+}
